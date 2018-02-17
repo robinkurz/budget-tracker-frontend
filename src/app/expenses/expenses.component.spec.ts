0174c3bec3be1c8cb3020f80ac3a95b1d06f523d
Robin Kurz <robin.kurz@protonmail.com>
2018-02-17 12:03:42 +0100

    wrote first unit test in Angular -> calculateTotal()

--- src/app/expenses/expenses.component.spec.ts
+++ src/app/expenses/expenses.component.spec.ts
@@ -5,6 +5,7 @@ import {MoneyPipe} from '../money.pipe';
 import {FormsModule} from '@angular/forms';
 import {ExpenseService} from '../expense.service';
 import {HttpClient, HttpHandler} from '@angular/common/http';
+import {Expense} from '../expense';
 
 describe('ExpensesComponent', () => {
   let component: ExpensesComponent
@@ -37,4 +38,23 @@ describe('ExpensesComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should calculate 50', () => {
+    const expense30 = new Expense();
+    expense30.amount = 30;
+    const expense20 = new Expense();
+    expense20.amount = 20;
+    component.expenses = [expense30, expense20];
+    expect(component.calculateTotal()).toBe(50);
+  });
+
+  it('should calculate -20', () => {
+    const expense30 = new Expense();
+    expense30.amount = 30;
+    const expenseNegative50 = new Expense();
+    expenseNegative50.amount = -50;
+    component.expenses = [expense30, expenseNegative50];
+    expect(component.calculateTotal()).toBe(-20);
+  });
+
 });
